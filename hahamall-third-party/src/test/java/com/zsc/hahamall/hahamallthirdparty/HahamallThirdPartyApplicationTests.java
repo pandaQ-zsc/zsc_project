f96f58b7e6c8d4ae0f63d7dec8b7e9805f993940
zsc_xq <469712814@qq.com>
2021-06-05 09:32:15 +0800

    自定义后台验证器

---
+++ hahamall-third-party/src/test/java/com/zsc/hahamall/hahamallthirdparty/HahamallThirdPartyApplicationTests.java
@@ -0,0 +1,49 @@
+package com.zsc.hahamall.hahamallthirdparty;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.PutObjectRequest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+
+@SpringBootTest
+public class HahamallThirdPartyApplicationTests {
+
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+     public void testUpLoad() {
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-shenzhen.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tFJb89A4yKKZZNq558U";
+        String accessKeySecret = "Q0w76qD0JayUKRgTZlf6asAyGFUGHV";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 创建PutObjectRequest对象。
+        // 填写Bucket名称、Object完整路径和本地文件的完整路径。Object完整路径中不能包含Bucket名称。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
+        PutObjectRequest putObjectRequest = new PutObjectRequest("hahamall-hello", "hahaha.jpg", new File("C:\\Users\\xxx\\Pictures\\xq_star.jpg"));
+        // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+        // ObjectMetadata metadata = new ObjectMetadata();
+        // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+        // metadata.setObjectAcl(CannedAccessControlList.Private);
+        // putObjectRequest.setMetadata(metadata);
+
+        // 上传文件。
+        ossClient.putObject(putObjectRequest);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+
+}
