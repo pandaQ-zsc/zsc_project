33c339402c7fd6f15a5a0af48770a82851865364
zsc_xq <469712814@qq.com>
2022-03-19 14:13:26 +0800

    search服务完善

---
+++ hahamall-auth-server/src/test/java/com/zsc/hahamall/auth/HahamallAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.zsc.hahamall.demo;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HahamallAuthServerApplicationTests {
+//
+//@Test
+//	void contextLoads() {
+//	}
+//
+//}
